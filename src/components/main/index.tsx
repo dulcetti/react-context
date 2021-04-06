943f0af06a3b32634a90b204cbfc61013a25be3d
Bruno Dulcetti <brunodulcetti@gmail.com>
2021-04-06 10:18:55 -0300

    feat: add initial main config

---
+++ src/components/main/index.tsx
@@ -0,0 +1,16 @@
+export default function App() {
+  return (
+    <div>
+      <header>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
