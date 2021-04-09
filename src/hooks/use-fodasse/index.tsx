8fd4f54513fb71db3388d361038fac8485de8bd1
Bruno Dulcetti <brunodulcetti@gmail.com>
2021-04-08 23:20:52 -0300

    feat: add default values for themes

--- src/hooks/use-fodasse/index.tsx
+++ src/hooks/use-fodasse/index.tsx
@@ -7,3 +7,14 @@ type IThmemesValues = {
   bg: string;
   color: string;
 };
+
+const themes: IThemes = {
+  dark: {
+    bg: '#333',
+    color: '#e6e7e7',
+  },
+  light: {
+    bg: '#e6e7e7',
+    color: '#333',
+  },
+};
