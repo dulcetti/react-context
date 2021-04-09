78273541ac8dba52c295d499a50c3b13e126476e
Bruno Dulcetti <brunodulcetti@gmail.com>
2021-04-08 23:38:57 -0300

    feat: add app styles and context

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,15 +1,29 @@
+import { useState } from 'react';
+
 import * as S from './styles';
 
+import { themes, ThemesContext } from '../../contexts/themes';
 import Footer from '../footer';
 import Content from '../content';
 import Header from '../header';
 
 export default function Main() {
+  const [theme, setTheme] = useState(themes.dark);
+
+  const appStyle = {
+    backgroundColor: theme.bg,
+    color: theme.color,
+    height: '100vh',
+    margin: '0',
+  };
+
   return (
-    <S.AppWrapper>
-      <Header />
-      <Content />
-      <Footer />
-    </S.AppWrapper>
+    <ThemesContext.Provider value={theme}>
+      <S.AppWrapper style={appStyle}>
+        <Header />
+        <Content />
+        <Footer />
+      </S.AppWrapper>
+    </ThemesContext.Provider>
   );
 }
