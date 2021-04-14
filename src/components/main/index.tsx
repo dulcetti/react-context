f968b83eee9f0f1660968ee88716619d80258bf0
Bruno Dulcetti <brunodulcetti@gmail.com>
2021-04-13 22:59:39 -0300

    feat: add toggleTheme on main

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -10,6 +10,10 @@ import Header from '../header';
 export default function Main() {
   const [theme, setTheme] = useState(themes.theme);
 
+  const toggleTheme = () => {
+    setTheme((theme) => (theme === 'dark' ? 'light' : 'dark'));
+  };
+
   const appStyle = {
     backgroundColor: themes[theme]?.bg,
     color: themes[theme]?.color,
@@ -18,7 +22,7 @@ export default function Main() {
   };
 
   return (
-    <ThemesContext.Provider value={{ theme }}>
+    <ThemesContext.Provider value={{ theme, toggleTheme }}>
       <S.AppWrapper style={appStyle}>
         <Header />
         <Content />
